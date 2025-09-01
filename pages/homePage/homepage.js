dd996198852c41f8108d6c2f6c36505f73b7464b
melskrm <melissakerim20@gmail.com>
2025-09-01 15:24:58 +0300

    JU-23

--- pages/homePage/homepage.js
+++ pages/homePage/homepage.js
@@ -43,3 +43,43 @@ searchBtn.addEventListener("click", () => {
 
 
 //melisa navbar section end
+
+
+
+//melisa discount banner section start 
+
+//geri sayımın biteceği zaman
+let deadLine= new Date("2025-09-30");
+let now= new Date("2025-09-01");
+
+ //  Geri sayımı güncelleyen fonksiyon
+  function updateCountdown() {
+    let now = new Date().getTime();
+    let diff = deadLine - now;
+
+    if (diff <= 0) {
+      document.getElementById("days").innerText = "00";
+      document.getElementById("hours").innerText = "00";
+      document.getElementById("minutes").innerText = "00";
+      document.getElementById("seconds").innerText = "00";
+      return;
+    }
+
+//milisaniye oalrak aldığımız için bunu gün ay vs dönüştürmemiz lazım
+
+let days=Math.floor(diff/(1000*60*60*24));
+let hours=Math.floor((diff%(1000*60*60*24))/(1000*60*60));
+let minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+let seconds=Math.floor((diff%(1000*60))/1000);
+
+
+//değerleri ekrana yazıdrma
+document.getElementById("days").innerText=days;
+document.getElementById("hours").innerText=hours;
+document.getElementById("minutes").innerText=minutes;
+document.getElementById("seconds").innerText=seconds;
+  }
+
+//her saniye güncelleöme
+setInterval(updateCountdown,1000);
+updateCountdown();
